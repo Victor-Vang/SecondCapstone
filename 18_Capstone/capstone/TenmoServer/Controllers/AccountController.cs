ba4b0ddb2545284fe99c0cba7b4bdae6d1b1f510
Victor Vang <vv.fitness614@gmail.com>
2022-03-09 14:37:50 -0500

    uh-huh

---
+++ 18_Capstone/capstone/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.DAO;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountDao accountDAO;
+        public AccountController(IAccountDao accountDAO)
+        {
+            this.accountDAO = accountDAO;
+        }
+
+        // GET: /account/userId
+        [HttpGet("{userId}")]
+        public ActionResult<Account> GetAccount(int userId)
+        {
+            Account account = accountDAO.GetAccount(userId);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(account);
+            }
+        }
+    }
+}
